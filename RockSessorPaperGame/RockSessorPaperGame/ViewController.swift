29cf5ccc440235f89f13df52beb45d451053445d
kio <un19451024@gmail.com>
2021-10-05 13:41:25 +0900

    Feat: viewDidLoad() 가위바위보 초기 설정

--- RockSessorPaperGame/RockSessorPaperGame/ViewController.swift
+++ RockSessorPaperGame/RockSessorPaperGame/ViewController.swift
@@ -19,16 +19,33 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 1. 첫번째/두번째 이미지뷰에 준비 이미지 띄우기
+        computerImageView.image = #imageLiteral(resourceName: "ready")
+        myImageView.image = UIImage(named: "ready.png")
+        
+        // 2. 첫번째/두번째 레이블에 "준비" 라고 띄우기
+        computerChoiceLabel.text = "준비"
+        myChoiceLabel.text = "준비"
     }
     
     @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        // 선택한 가위 or 바위 or 보 를 저장하기
     }
         
     @IBAction func selectButtonTapped(_ sender: UIButton) {
+        // 1. 컴퓨터가 랜덤 선택한 것을 이미지 뷰에 표시하기
+        // 2. 컴퓨터가 랜덤 선택한 것을 레이블에 표시하기
+        // 3. 내가 선택한 것을 이미지 뷰에 표시하기
+        // 4. 내가 선택한 것을 레이블에 표시하기
+        // 5. 선택한 정보를 바탕으로 컴퓨터가 선택한 정보랑 비교해서 이겼는지 졌는지 mainLabel에 출력하기
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        // 1. 컴퓨터 다시 준비상태 이미지 뷰에 표시하기
+        // 2. 컴퓨터 다시 준비상태 레이블에 표시하기
+        // 3. 내 선택 이미지 뷰도 준비상태 표시하기
+        // 4. 내 선택 레이블도 준비상태 표시하기
+        // 5. mainLabel에 "선택하세요" 표시
     }
 }
 
