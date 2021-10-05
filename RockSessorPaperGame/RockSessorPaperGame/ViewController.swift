17a5687acdf0afbb8068906f40744a8b5f3f49a2
kio <un19451024@gmail.com>
2021-10-05 13:20:44 +0900

    Feat: ViewController에 스토리보드 label, imageView, Button 연결하기

--- RockSessorPaperGame/RockSessorPaperGame/ViewController.swift
+++ RockSessorPaperGame/RockSessorPaperGame/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var computerImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var computerChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+    }
+        
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
 }
 
